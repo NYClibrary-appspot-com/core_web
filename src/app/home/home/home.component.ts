b4cba57a390aa6ddeb48a967571bfea4f3601b39
Ali Almashhadani <ali.almashhadani89@gmail.com>
2019-11-12 20:44:00 -0500

    added the repo link

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,27 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { LibrayServicesService } from 'src/app/services/libray-services.service';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { LibrayServicesService } from "src/app/services/libray-services.service";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"]
 })
 export class HomeComponent implements OnInit {
+  constructor(
+    private router: Router,
+    private libService: LibrayServicesService
+  ) {}
 
-  constructor(private router : Router, private libService : LibrayServicesService) { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  test() {
+    this.libService.welcomeMessage().subscribe((data: any) => {
+      console.log(data);
+    });
   }
 
-  
-
-  addBook(){
-    this.router.navigate(['/home/addBook']);
+  addBook() {
+    this.router.navigate(["/home/addBook"]);
   }
 
-  bookList(){
-    this.router.navigate(['/home/bookList']);
+  bookList() {
+    this.router.navigate(["/home/bookList"]);
   }
-
 }
--- src/app/services/libray-services.service.ts
+++ src/app/services/libray-services.service.ts
@@ -1,31 +1,39 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpErrorResponse, HttpParams } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+import {
+  HttpClient,
+  HttpHeaders,
+  HttpErrorResponse,
+  HttpParams
+} from "@angular/common/http";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class LibrayServicesService {
   searchSkills(query: string): import("rxjs").ObservableInput<unknown> {
     throw new Error("Method not implemented.");
   }
 
-  readonly rootUrl = 'https://www.google.com/';
-  constructor(private http: HttpClient) { }
+  readonly rootUrl = "https://back-end-258822.appspot.com";
+  constructor(private http: HttpClient) {}
 
-
-  hiGoogle(){   // get request
-    return this.http.get( this.rootUrl);
+  welcomeMessage() {
+    // get request
+    return this.http.get(this.rootUrl);
   }
 
-  get_booklist(){      // get request
-    return this.http.get( this.rootUrl +"book_list");
+  get_booklist() {
+    // get request
+    return this.http.get(this.rootUrl + "book_list");
   }
-  
-  search_a_book(book_name:string){  //get request
+
+  search_a_book(book_name: string) {
+    //get request
     return null;
   }
 
-  add_a_book(book_name:string){ //post request
+  add_a_book(book_name: string) {
+    //post request
     return null;
   }
 }
