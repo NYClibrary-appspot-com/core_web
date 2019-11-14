3659643594098699fca7fa97dda7dcd8390e819f
Ali Almashhadani <ali.almashhadani89@gmail.com>
2019-11-14 15:36:27 -0500

    fixed the style

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Inject } from "@angular/core";
 import { Router } from "@angular/router";
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { FormGroup, FormControl, FormsModule } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material/dialog";
+import { FormGroup, FormControl, FormsModule } from "@angular/forms";
 import { LibrayServicesService } from "src/app/services/libray-services.service";
 
 @Component({
@@ -12,32 +12,28 @@ import { LibrayServicesService } from "src/app/services/libray-services.service"
 export class HomeComponent implements OnInit {
   //default variable for selected file.
   fileToUpload: File = null;
-  book_name; 
+  book_name;
   formdata;
-
-
 
   constructor(
     private router: Router,
-    private libService: LibrayServicesService,
-   ) {}
+    private libService: LibrayServicesService
+  ) {}
 
-
-
-  ngOnInit() { 
+  ngOnInit() {
     this.formdata = new FormGroup({});
   }
 
   handleFileInput(files: FileList) {
     this.fileToUpload = files.item(0);
-}
+  }
 
-uploadFileToActivities(file : any){
-    console.log(typeof file)
+  uploadFileToActivities(file: any) {
+    console.log(typeof file);
     this.libService.add_a_book(file).subscribe((data: any) => {
       console.log(data);
-    })
-    ;}
+    });
+  }
 
   test() {
     this.libService.welcomeMessage().subscribe((data: any) => {
@@ -45,23 +41,19 @@ uploadFileToActivities(file : any){
     });
   }
 
-  
-
-
   bookList() {
     this.libService.get_booklist().subscribe((data: any) => {
       console.log(data);
     });
   }
 
-  searchBook(book_name: string){
-    this.libService.search_a_book(Object(book_name)["book_name"]).subscribe((data: any) => {
-      console.log(data);
-    });
-
+  searchBook(book_name: string) {
+    this.libService
+      .search_a_book(Object(book_name)["book_name"])
+      .subscribe((data: any) => {
+        console.log(data);
+      });
   }
-
-
 
   addBook(book_name: File) {
     // this.libService.add_a_book(book_name).subscribe((data: any) => {
@@ -69,7 +61,4 @@ uploadFileToActivities(file : any){
     // });
     this.router.navigate(["/home"]);
   }
-
-
-
 }
