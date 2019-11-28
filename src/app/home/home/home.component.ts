e46463d306d0c6b0ad259154b1ca803efffe3cea
EugeneSajor43 <eugenesajor43@myhunter.cuny.edu>
2019-11-27 19:58:54 -0500

    For Loop only prints 5 books

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -36,6 +36,8 @@ export class HomeComponent implements OnInit {
   bookList() {
     this.libService.get_booklist().subscribe((data: any) => {
       this.book_list = data;
+
+
       
       // console.log(this.book_list);
     });
