b90a2eeac040e7d70a155239ac51de599fef25ec
Tushar Malakar <35859780+TusharMalakar@users.noreply.github.com>
2019-12-01 17:39:27 -0500

    Dockerfile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule, CanActivate } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 
 /**
 * @author Tushar Malakar
